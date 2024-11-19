ee88f90412a507cd3eecd27979b5ab1c87db86e4
GiovannaLani <giovanna.lani@opendeusto.es>
2024-11-19 12:56:42 +0100

    Primera plantilla
    Co-Authored-By: Patricia Rodríguez Sánchez <146166220+PatriciaRdrz@users.noreply.github.com>
    Co-Authored-By: Joana Artetxe Goti <145749032+jArtetxe@users.noreply.github.com>

--- proyectoGrupo3/winxAPP/urls.py
+++ proyectoGrupo3/winxAPP/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='listaE'),
+    path('', views.index, name='index'),
     path('temporadas/', views.listaTemporadas, name='listaTemporadas'),
     path('temporadas/<int:id_temporada>/', views.detalleTemporada, name='detalleTemporada'),
     path('escuelas/', views.listaEscuelas, name='listaEscuelas'),
